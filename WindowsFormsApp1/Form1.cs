1b2e3d0cd6513c2d6fbffb705c422ad3d07eeb9a
masyan455 <109448816+masyan455@users.noreply.github.com>
2022-07-31 13:25:00 +0700

    Add project files.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string content = "MR Akkarawat";
+            string title = "Say my name";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+
+            DialogResult result = MessageBox.Show(content, title, buttons);
+
+            if (result == DialogResult.Yes)
+            {
+                //ถ้าตอบใช้จะเกิดอะไรขึ้น
+                MessageBox.Show("ขอบคุณ", title, MessageBoxButtons.OK);
+            }
+            else
+            {
+                MessageBox.Show("ขออภัยนะครับ", title, MessageBoxButtons.OK);
+            }
+        }
+    }
+}
